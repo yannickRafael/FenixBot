d1ac334eca698e3dd2c70d75ed9639cc8e653d0f
yannickRafael <yannickrafaek286@gmail.com>
2023-07-05 11:55:38 +0200

    1. Adicionei o ficheiro cadeiras.xlsx que contem dados de todas cadeiras da faculdade 2. Adicioneu o searchBot.py que busca as infos da cadeira e aluno 2. adicionei o getMarksBot.py que busca as notas do alunno

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 account_sid = 'ACa5c83e45677a24409033440f8499727b'
 auth_token = 'c1b4fb1c0d36280276b9f8a0b6a68cf2'
-invalid_comand_error = 'Comando não reconhecido\nFormato de comando: [curso]/[cadeira]/[nr de estudante]'
+invalid_comand_error = 'Comando não reconhecido\nFormato de comando:\n    [curso]/[cadeira]/[nr de estudante]'
+username = 'yannick.matimbe'
+password = 'Y@nnick2003'
+login_url = 'https://fenix.isutc.ac.mz/cas/login?service=https://fenix.isutc.ac.mz/isutc/'
+logged_url = 'https://fenix.isutc.ac.mz/isutc/fenixEduIndex.do'
+mark_url = 'https://fenix.isutc.ac.mz/isutc/publico/executionCourse.do?method=marks&executionCourseID=1126312223704031'
 
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from config import *
-
+import searchBot as bot
 from twilio.rest import Client
 from flask import Flask, request, jsonify
 
@@ -10,6 +10,20 @@ def fill(string):
     else:
         return 'whatsapp:' + str(string)
 
+def format_answer(primeira_linha, linhas_encontradas, ultima_linha):
+    ans = []
+    for i in range(0, len(linhas_encontradas)):
+        if i >= 2:
+            text = linhas_encontradas[i] + ': ' + primeira_linha[i] + '/' + ultima_linha[i - 1]
+            ans.append(text)
+        if i < 2:
+            text = linhas_encontradas[i] + ': ' + primeira_linha[i]
+            ans.append(text)
+
+    text = ""
+    for i in ans:
+        text = text + i + '\n'
+    return text
 
 def send(message, number):
     client = Client(account_sid, auth_token)
@@ -35,6 +49,11 @@ def bot():
         send(invalid_comand_error, number)
     else:
         send(f'procurando notas de {keys[2]} da cadeira {keys[1]}', number)
+        primeira_linha, linhas_encontradas, ultima_linha = bot.getNotas(keys[0], keys[1], keys[2])
+
+        answer = format_answer(primeira_linha, linhas_encontradas, ultima_linha)
+        send(answer, number)
+
 
     return jsonify({'message': 'Success'})
 
