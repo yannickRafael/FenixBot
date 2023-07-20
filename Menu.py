c29f87937d13755b0282627cab9ee52cb6b477d2
yannickRafael <yannickrafaek286@gmail.com>
2023-07-20 21:36:14 +0200

    added Menu.py

---
+++ Menu.py
@@ -0,0 +1,26 @@
+class Menu:
+    def __init__(self, name, prompt, options):
+        self.name = name
+        self.prompt = prompt
+        self.options = options
+        self.range = []
+        for i in range(0, len(options)):
+            self.range.append(str(i+1))
+
+
+
+    def print_prompt(self):
+        text = self.prompt + ': \n'
+        for i in range(0, len(self.options)):
+            text = text + str(1 + i) + '. ' + self.options[i] + '\n'
+        return text
+
+    def select_data(self, message):
+        try:
+            i = int(message)
+            if 1 <= i <= len(self.options):
+                return self.options[i - 1]
+            else:
+                return 'Opção inválida'
+        except:
+            return 'Opção inválida'
